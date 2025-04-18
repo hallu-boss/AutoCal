901cee4b90619061497472117d0459971a085140
Paweł Hase <pawelhase@gmail.com>
2025-04-19 00:19:10 +0200

    Added constants to shared

---
+++ shared/constants.py
@@ -0,0 +1,14 @@
+from typing import Final
+
+DATA_QUEUE_NAME: Final[str] = "data_queue"
+GOOGLE_API_QUEUE_NAME: Final[str] = "google_api_queue"
+
+RESPONSE_PREFIX: Final[str] = "RES:"
+
+WEEKS_HEADERS: Final[tuple] = ("t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11", "t12", "t13", "t14", "t15")
+
+WEEK_DAYS_HEADERS: Final[tuple] = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+YEAR_HEADER: Final[str] = "year"
+
+OCCURRENCES_HEADER: Final[str] = "occurrences"
